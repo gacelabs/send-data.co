a38e30cf0acbc10e7b695b5465c006bf44965ac7
Bong Garcia <gacelabs.inc@gmail.com>
2018-11-05 14:12:13 +0800

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,27 +16,29 @@ $(document).ready(function() {
 		parent.find('#'+targetId).toggleClass('toggle-hide');
 	});
 
-	$('#re-pw-name').on('input', function(e) {
-		if ($(this).val() != $('#pw-name').val()) {
-			$(this).closest('form').find('button').removeClass('btn-success').addClass('btn-danger').css('pointer-events', 'none');
-			$(this).addClass('input-error');
-			$('#pw-name').addClass('input-error');
-		} else {
-			$(this).closest('form').find('button').removeClass('btn-danger').addClass('btn-success').css('pointer-events', 'auto');
-			$(this).removeClass('input-error');
-			$('#pw-name').removeClass('input-error');
-		}
-	});
-	$('#pw-name').on('input', function(e) {
-		if ($(this).val() != $('#re-pw-name').val()) {
-			$(this).closest('form').find('button').removeClass('btn-success').addClass('btn-danger').css('pointer-events', 'none');
-			$(this).addClass('input-error');
-			$('#re-pw-name').addClass('input-error');
-		} else {
-			$(this).closest('form').find('button').removeClass('btn-danger').addClass('btn-success').css('pointer-events', 'auto');
-			$(this).removeClass('input-error');
-			$('#re-pw-name').removeClass('input-error');
-		}
-	});
+	if ($('#login-form').length == 0) {
+		$('#re-pw-name').on('input', function(e) {
+			if ($(this).val() != $('#pw-name').val()) {
+				$(this).closest('form').find('button').removeClass('btn-success').addClass('btn-danger').css('pointer-events', 'none');
+				$(this).addClass('input-error');
+				$('#pw-name').addClass('input-error');
+			} else {
+				$(this).closest('form').find('button').removeClass('btn-danger').addClass('btn-success').css('pointer-events', 'auto');
+				$(this).removeClass('input-error');
+				$('#pw-name').removeClass('input-error');
+			}
+		});
+		$('#pw-name').on('input', function(e) {
+			if ($(this).val() != $('#re-pw-name').val()) {
+				$(this).closest('form').find('button').removeClass('btn-success').addClass('btn-danger').css('pointer-events', 'none');
+				$(this).addClass('input-error');
+				$('#re-pw-name').addClass('input-error');
+			} else {
+				$(this).closest('form').find('button').removeClass('btn-danger').addClass('btn-success').css('pointer-events', 'auto');
+				$(this).removeClass('input-error');
+				$('#re-pw-name').removeClass('input-error');
+			}
+		});
+	}
 
 });
