3a0c7e340dda79dc1fe798a4d27f3d86202106a9
Bong Garcia <gacelabs.inc@gmail.com>
2018-11-05 12:03:13 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,4 +16,27 @@ $(document).ready(function() {
 		parent.find('#'+targetId).toggleClass('toggle-hide');
 	});
 
+	$('#re-pw-name').on('input', function(e) {
+		if ($(this).val() != $('#pw-name').val()) {
+			$(this).closest('form').find('button').css('pointer-events', 'none');
+			$(this).addClass('input-error');
+			$('#pw-name').addClass('input-error');
+		} else {
+			$(this).closest('form').find('button').css('pointer-events', 'auto');
+			$(this).removeClass('input-error');
+			$('#pw-name').removeClass('input-error');
+		}
+	});
+	$('#pw-name').on('input', function(e) {
+		if ($(this).val() != $('#re-pw-name').val()) {
+			$(this).closest('form').find('button').css('pointer-events', 'none');
+			$(this).addClass('input-error');
+			$('#re-pw-name').addClass('input-error');
+		} else {
+			$(this).closest('form').find('button').css('pointer-events', 'auto');
+			$(this).removeClass('input-error');
+			$('#re-pw-name').removeClass('input-error');
+		}
+	});
+
 });
