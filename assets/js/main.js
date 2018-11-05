50150b5358a232727042a3e9693b5e0ee1e23720
Bong Garcia <gacelabs.inc@gmail.com>
2018-11-05 12:07:24 +0800

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,22 +18,22 @@ $(document).ready(function() {
 
 	$('#re-pw-name').on('input', function(e) {
 		if ($(this).val() != $('#pw-name').val()) {
-			$(this).closest('form').find('button').css('pointer-events', 'none');
+			$(this).closest('form').find('button').removeClass('btn-success').addClass('btn-danger').css('pointer-events', 'none');
 			$(this).addClass('input-error');
 			$('#pw-name').addClass('input-error');
 		} else {
-			$(this).closest('form').find('button').css('pointer-events', 'auto');
+			$(this).closest('form').find('button').removeClass('btn-danger').addClass('btn-success').css('pointer-events', 'auto');
 			$(this).removeClass('input-error');
 			$('#pw-name').removeClass('input-error');
 		}
 	});
 	$('#pw-name').on('input', function(e) {
 		if ($(this).val() != $('#re-pw-name').val()) {
-			$(this).closest('form').find('button').css('pointer-events', 'none');
+			$(this).closest('form').find('button').removeClass('btn-success').addClass('btn-danger').css('pointer-events', 'none');
 			$(this).addClass('input-error');
 			$('#re-pw-name').addClass('input-error');
 		} else {
-			$(this).closest('form').find('button').css('pointer-events', 'auto');
+			$(this).closest('form').find('button').removeClass('btn-danger').addClass('btn-success').css('pointer-events', 'auto');
 			$(this).removeClass('input-error');
 			$('#re-pw-name').removeClass('input-error');
 		}
