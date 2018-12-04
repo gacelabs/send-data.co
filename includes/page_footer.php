79d0c7fbd18c5a7d60b54d45e630cf602d767eeb
Bong Garcia <gacelabs.inc@gmail.com>
2018-12-04 21:47:36 +0800

    added contact us links

--- includes/page_footer.php
+++ includes/page_footer.php
@@ -1,6 +1,6 @@
 <footer class="py-5 bg-dark">
 	<div class="container">
-		<p class="m-0 text-center text-white"><a href="mailto:gacelabs.inc@gmail.com">Contact Us</a></p>
+		<p class="m-0 text-center text-white"><a href="mailto:gacelabs.inc@gmail.com?subject=Enquiry&body=Full%20name%3A%20%0D%0AEmail%3A%20%0D%0AMessage%3A%20%0D%0A%0D%0A%0D%0A%0D%0A">Contact Us</a></p>
 		<p class="m-0 text-center text-white">DataPushThru&copy; <span class="yearNow"></span></p>
 	</div>
 	<!-- /.container -->
--- includes/page_nav.php
+++ includes/page_nav.php
@@ -13,7 +13,7 @@
 					<a class="nav-link" href="?page=documentation">Docs</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="mailto:gacelabs.inc@gmail.com">Contact Us</a>
+					<a class="nav-link" href="mailto:gacelabs.inc@gmail.com?subject=Enquiry&body=Full%20name%3A%20%0D%0AEmail%3A%20%0D%0AMessage%3A%20%0D%0A%0D%0A%0D%0A%0D%0A">Contact Us</a>
 				</li>
 				<li class="nav-item <?php if ($activePage == 'login') {echo 'active';}; ?>">
 					<a class="nav-link" href="?page=login">Login</a>
