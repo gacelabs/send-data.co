862227a752edd0e2acc9bf2c7ce2a94d7c6c7349
Eddie Garcia <eddie@ypdigital.com.au>
2019-02-10 20:25:04 +0800

    update text

--- includes/array.php
+++ includes/array.php
@@ -25,7 +25,7 @@
 			'desc' => array(
 				'Monthly',
 				'20K Upfront Payload for Php 7K',
-				'Metered Succeeding Payload',
+				'Php 1K Metered Succeeding Payload',
 				'24/7 Email Support'
 			)
 		),
