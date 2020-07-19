393588815d2469f0aca6ed43dac37627bf6bc40f
Eddie <eddie@ypdigital.com.au>
2020-07-19 18:02:15 +0800

    push

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('includes/page_head.php');
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
 	<!-- End Google Tag Manager (noscript) -->
 	<!-- Load Facebook SDK for JavaScript -->
-	<!-- <div id="fb-root"></div>
+	<div id="fb-root"></div>
 	<script>
 		window.fbAsyncInit = function() {
 			FB.init({
@@ -26,10 +26,10 @@ include('includes/page_head.php');
 			js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
-	</script> -->
+	</script>
 
 	<!-- Your customer chat code -->
-	<!-- <div class="fb-customerchat" attribution="setup_tool" page_id="307503806518656"></div> -->
+	<div class="fb-customerchat" attribution="setup_tool" page_id="307503806518656"></div>
 
 	<!-- Navigation -->
 	<?php
