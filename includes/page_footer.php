e76afb53f99368afec220e9985a7e0ca77e35f91
Eddie <gacelabs.inc@gmail.com>
2021-09-20 07:35:06 +0800

    revert

--- includes/page_footer.php
+++ includes/page_footer.php
@@ -15,8 +15,3 @@
 <script src="assets/js/highlight.pack.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
 <script src="assets/js/calc.js"></script>
-
-<div id="my-store-65639004"></div>
-<div>
-	<script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?65639004&data_platform=code&data_date=2021-09-20" charset="utf-8"></script><script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-65639004");</script>
-</div>
