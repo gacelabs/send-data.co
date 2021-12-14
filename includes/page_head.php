4f3620c19975ec785ae0a5599a7717d059e9d546
Eddie <gacelabs.inc@gmail.com>
2021-12-14 11:34:07 +0800

    up

--- includes/page_head.php
+++ includes/page_head.php
@@ -11,6 +11,7 @@
 			gtag('js', new Date());
 			gtag('config', 'UA-185827424-1');
 		</script> -->
+		<script data-ad-client="ca-pub-3716182521515143" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<?php endif ?>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
