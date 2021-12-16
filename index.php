4a14f53ffc6bfcd24323e6b945bf87cc301ce972
Eddie <gacelabs.inc@gmail.com>
2021-12-16 13:20:24 +0800

    up

--- index.php
+++ index.php
@@ -6,27 +6,33 @@ include('includes/page_head.php');
 ?>
 <body>
 	<?php if ((bool)strstr($_SERVER['HTTP_HOST'], 'local.') == false): ?>
-		<!-- Load Facebook SDK for JavaScript -->
 		<!-- Messenger Chat Plugin Code -->
 		<div id="fb-root"></div>
+
 		<!-- Your Chat Plugin code -->
-		<div id="fb-customer-chat" class="fb-customerchat"></div>
+		<div id="fb-customer-chat" class="fb-customerchat">
+		</div>
+
 		<script>
 			var chatbox = document.getElementById('fb-customer-chat');
 			chatbox.setAttribute("page_id", "307503806518656");
 			chatbox.setAttribute("attribution", "biz_inbox");
+		</script>
+
+		<!-- Your SDK code -->
+		<script>
 			window.fbAsyncInit = function() {
 				FB.init({
-					xfbml : true,
-					version : 'v12.0'
+					xfbml            : true,
+					version          : 'v12.0'
 				});
 			};
+
 			(function(d, s, id) {
 				var js, fjs = d.getElementsByTagName(s)[0];
 				if (d.getElementById(id)) return;
 				js = d.createElement(s); js.id = id;
 				js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-				// js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
 				fjs.parentNode.insertBefore(js, fjs);
 			}(document, 'script', 'facebook-jssdk'));
 		</script>
