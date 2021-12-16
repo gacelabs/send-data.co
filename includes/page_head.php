91bca06a6693f55d449be52bdd9080f34c916f09
Eddie <gacelabs.inc@gmail.com>
2021-12-16 13:22:13 +0800

    up

--- includes/page_head.php
+++ includes/page_head.php
@@ -21,8 +21,8 @@
 
 	<?php if ((bool)strstr($_SERVER['HTTP_HOST'], 'local.') == false): ?>
 		<!-- Facebook Pixel Code -->
-		<!-- <script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '1520941091422245');fbq('track', 'PageView');</script>
-		<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1520941091422245&ev=PageView&noscript=1"/></noscript> -->
+		<script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '1520941091422245');fbq('track', 'PageView');</script>
+		<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1520941091422245&ev=PageView&noscript=1"/></noscript>
 		<!-- End Facebook Pixel Code -->
 	<?php endif ?>
 </head>
