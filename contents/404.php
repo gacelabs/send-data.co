0bc6c592a792cc90e8133c42bd9bfe6469f45e14
Eddie <gacelabs.inc@gmail.com>
2021-12-31 18:21:53 +0800

    delete

--- contents/404.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-	<div class="form-container controlled center">
-		<div class="form-header text-center mb-3">
-			<h5 class="zero-gaps">404 PAGE NOT FOUND!</h5>
-		</div>
-	</div>
-</div>
--- contents/enquiry.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-	<div class="form-container controlled center" style="width: auto;">
-		<div class="form-header text-center mb-3">
-			<h5 class="zero-gaps">Your Enquiry has been sent, We will be responding to you soon, Thank you!</h5>
-		</div>
-	</div>
-</div>
